0f192b8af41e8666e8fde8645af40be19fd45a15
Gabriel Corrêa <gabrielcorrreaa@gmail.com>
2025-07-30 23:37:11 -0300

    Prepara o servidor para o deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/views', 'index.html'));
